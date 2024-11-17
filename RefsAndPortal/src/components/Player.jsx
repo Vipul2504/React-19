1667aaf6315fc214d626827570dcff6ea4f818ad
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2024-11-17 22:06:03 +0530

    M

--- RefsAndPortal/src/components/Player.jsx
+++ RefsAndPortal/src/components/Player.jsx
@@ -1,11 +1,18 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  const handleClick = () => {
+    setEnteredPlayerName(playerName.current.value);
+  };
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? " "}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
-        <button>Set Name</button>
+        <input type="text" ref={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
