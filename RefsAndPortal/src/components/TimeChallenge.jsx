b8c112dc9043bbabe2d3f96429d0d976cc456e96
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2024-11-20 22:27:26 +0530

    Time challege component

---
+++ RefsAndPortal/src/components/TimeChallenge.jsx
@@ -0,0 +1,38 @@
+import { useRef, useState } from "react";
+
+const TimeChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
+
+  const [timeStarted, setTimeStarted] = useState(false);
+  const [timeExpired, setTimeExpired] = useState(false);
+
+  const handleStart = () => {
+    setTimeStarted(true);
+    timer.current = setTimeout(() => {
+      setTimeExpired(true);
+    }, targetTime * 1000);
+  };
+
+  const handleStop = () => {
+    clearTimeout(timer.current);
+  };
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timeExpired && <p>You lost</p>}
+      <p className="challenge-time">
+        {targetTime} second {targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={timeStarted ? "handleStop" : "handleStart"}>
+          {timeStarted ? "Stop" : "Start"} Challenge
+        </button>
+      </p>
+      <p className={timeStarted ? "active" : undefined}>
+        {timeStarted ? "Time is running..." : "Timer inactive"}
+      </p>
+    </section>
+  );
+};
+
+export default TimeChallenge;
