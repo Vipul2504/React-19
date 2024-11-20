bfae037adf1ba4502945afdd1d2ad91192a8959a
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2024-11-20 22:27:12 +0530

    M

--- RefsAndPortal/src/App.jsx
+++ RefsAndPortal/src/App.jsx
@@ -1,10 +1,16 @@
 import Player from "./components/Player.jsx";
+import TimeChallenge from "./components/TimeChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimeChallenge title="Easy" targetTime={1} />
+        <TimeChallenge title="Medium" targetTime={3} />
+        <TimeChallenge title="Hard" targetTime={5} />
+        <TimeChallenge title="Pro Only" targetTime={10} />
+      </div>
     </>
   );
 }
