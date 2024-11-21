0abc10af1bbee9a7441010bcd09fb45bf0be4f7f
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2024-11-21 23:00:52 +0530

    C

--- RefsAndPortal/src/components/ResultModal.jsx
+++ RefsAndPortal/src/components/ResultModal.jsx
@@ -1,10 +1,29 @@
-const ResultModal = ({ result, targetTime }) => {
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+const ResultModal = forwardRef(({ result, targetTime }, ref) => {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
   return (
-    <dialog>
+    <dialog ref={ref} className="result-modal">
       <h2>you {result}</h2>
       <p>
         The target time was <strong>{targetTime}</strong>
       </p>
+      <p>
+        You stopped the timer with <strong>X second left.</strong>
+      </p>
+      <form action="" method="dialog">
+        <button>Close</button>
+      </form>
     </dialog>
   );
-};
+});
+
+export default ResultModal;
