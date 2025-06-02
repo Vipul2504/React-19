ad424f5a1ecaf657c10dede80058e74035a3df27
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-06-02 23:41:51 +0530

    M

--- Project_Management/src/components/ProjectSidebar.jsx
+++ Project_Management/src/components/ProjectSidebar.jsx
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 export const ProjectSidebar = () => {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-800 text-stone-50 md:w-72 rounded-r-xl">
@@ -5,9 +7,7 @@ export const ProjectSidebar = () => {
         Your Projects
       </h2>
       <div>
-        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-400 hover:text-stone-100">
-          + Add Projects
-        </button>
+        <Button>+ Add Project</Button>
       </div>
       <ul></ul>
     </aside>
