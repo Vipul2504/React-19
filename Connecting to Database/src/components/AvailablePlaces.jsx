1fecb51b353e7c25fe353e4d745d3abb45e09b1c
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-11-11 23:55:02 +0530

    Data fetched

--- Connecting to Database/src/components/AvailablePlaces.jsx
+++ Connecting to Database/src/components/AvailablePlaces.jsx
@@ -1,10 +1,26 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      LoadingText="Fetching the Places"
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
