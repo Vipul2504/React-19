09bec7005efe70f7ec64635a12fe12e6ebe0bf65
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-11-12 23:30:01 +0530

    Changes

--- Connecting to Database/src/components/AvailablePlaces.jsx
+++ Connecting to Database/src/components/AvailablePlaces.jsx
@@ -1,16 +1,31 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../https.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [errorMes, setErrorMes] = useState();
+
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
-      setIsFetching(false);
+      try {
+        const places = await fetchAvailablePlaces();
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places.data,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setErrorMes({ message: error.message || "could not fetch data" });
+        setIsFetching(false);
+      }
     }
 
     fetchPlaces();
