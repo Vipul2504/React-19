5989894204b43b58778a0dbe0794c550d113dc35
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-11-12 23:29:55 +0530

    Functions

---
+++ Connecting to Database/src/https.js
@@ -0,0 +1,26 @@
+export async function fetchAvailablePlaces() {
+  const response = await fetch("http://localhost:3000/places");
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to fetch places");
+  }
+
+  return resData;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/places", {
+    method: "PUT",
+    body: JSON.stringify(places),
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to Update user data");
+  }
+
+  return resData;
+}
