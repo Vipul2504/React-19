1b57becc3b45613c7d11e380fc67b1c1ad79f4e5
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-11-25 22:14:02 +0530

    M

--- Foodista/src/App.jsx
+++ Foodista/src/App.jsx
@@ -1,9 +1,11 @@
+import { Header } from "./component/Header";
+import { Meal } from "./component/Meal";
+
 function App() {
   return (
     <>
-      <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
+      <Header />
+      <Meal />
     </>
   );
 }
---
+++ Foodista/src/utils/formatting.js
@@ -0,0 +1,4 @@
+export const formattingCurrency = new Intl.NumberFormat("de-DE", {
+  style: "currency",
+  currency: "USD",
+});
