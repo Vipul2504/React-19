896fee883b7e65495b21e8bb517a64d5f99a782a
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-12-01 23:12:35 +0530

    M

--- Foodista/src/component/Header.jsx
+++ Foodista/src/component/Header.jsx
@@ -2,13 +2,19 @@ import React, { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import { Button } from "./UI/Button";
 import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContex";
 
 export const Header = () => {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
   }, 0);
+
+  const handleShowCart = () => {
+    userProgressCtx.showCart();
+  };
   return (
     <header id="main-header">
       <div id="title">
@@ -16,7 +22,9 @@ export const Header = () => {
         <h1>React Food</h1>
       </div>
       <nav>
-        <Button textOnly>Cart {totalCartItems}</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart {totalCartItems}
+        </Button>
       </nav>
     </header>
   );
