cd32f03f0e4117510890f482c4bc361fa7f3f979
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-12-09 23:39:26 +0530

    repalce reducer

--- Redux-Advance/src/store/cart-slice.js
+++ Redux-Advance/src/store/cart-slice.js
@@ -1,9 +1,16 @@
-const { createSlice } = require("@reduxjs/toolkit");
+import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
   name: "cart",
-  initialState: { items: [], totalQuantity: 0 },
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -29,7 +36,6 @@ const cartSlice = createSlice({
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
