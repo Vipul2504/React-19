45df29916998a5f6f43a1d672e6bde8639b49e0a
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-12-16 21:10:41 +0530

    back path

--- Routing/src/pages/ProductDetails.jsx
+++ Routing/src/pages/ProductDetails.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, Links, useParams } from "react-router-dom";
 
 const ProductDetails = () => {
   const param = useParams();
   return (
     <>
       <h1>Product Details</h1>
-      <div>{param.productId}</div>;
+      <div>{param.productId}</div>
+      <p>
+        <Link to="" relative="path">
+          Back
+        </Link>
+      </p>
     </>
   );
 };
