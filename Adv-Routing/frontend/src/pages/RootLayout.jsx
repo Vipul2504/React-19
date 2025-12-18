af98dc7d199a5054b59cab7c201663af3409216c
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-12-18 23:47:06 +0530

    root

---
+++ Adv-Routing/frontend/src/pages/RootLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router";
+
+const RootLayout = () => {
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default RootLayout;
