03e8a674ec7c1bee36b8eeb5cbb15674e7d00205
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-12-19 22:02:51 +0530

    M

--- Adv-Routing/frontend/src/App.js
+++ Adv-Routing/frontend/src/App.js
@@ -1,19 +1,3 @@
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
 // 5. Ensure that the links in MainNavigation receive an "active" class when active
 // 6. Output a list of dummy events to the EventsPage
 //    Every list item should include a link to the respective EventDetailPage
@@ -25,7 +9,7 @@ import NewEventPage from "./pages/NewEventPage";
 import EventDetailPage from "./pages/EventDetailPage";
 import EditEventPage from "./pages/EditEventPage";
 import EventPage from "./pages/EventPage";
-
+import HomePage from "./pages/HomePage";
 const router = createBrowserRouter([
   {
     path: "/",
