278bab0087b0322baaa829b1c1617f3fd22bdaf0
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2025-12-27 23:38:56 +0530

    Added method prop

--- Adv-Routing/frontend/src/pages/NewEventPage.jsx
+++ Adv-Routing/frontend/src/pages/NewEventPage.jsx
@@ -2,36 +2,7 @@ import { redirect } from "react-router";
 import EventForm from "../components/EventForm";
 
 const NewEventPage = () => {
-  return <EventForm />;
+  return <EventForm method="POST" />;
 };
 
 export default NewEventPage;
-
-export async function action({ request, params }) {
-  const data = await request.formData();
-
-  const eventData = {
-    title: data.get("title"),
-    image: data.get("image"),
-    description: data.get("description"),
-    date: data.get("date"),
-  };
-
-  const response = await fetch("http://localhost:8080/events", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(eventData),
-  });
-
-  if (response.status === 422) {
-    return response;
-  }
-
-  if (!response.ok) {
-    throw JSON({ message: "Could not save event" }, { status: 500 });
-  }
-
-  return redirect("/event");
-}
